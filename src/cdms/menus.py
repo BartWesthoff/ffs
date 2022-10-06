04d07dbbd98c78fbb39551298a0682c54f2385e4
bartw <bartwesthoff2002@gmail.com>
2022-10-06 19:31:00 +0200

    kwargs toegevoegd

--- src/cdms/menus.py
+++ src/cdms/menus.py
@@ -35,16 +35,17 @@ class Action:
 def menu(user_access_level: int):
     # TODO: alles wat comment is moeten we nog (af)maken verder werkt systeem goed.
     from src.cdms.userinterfaceClass import UserInterface, Role
+    crud = Crud()
     """ TO DEBUG: menu(Role.SUPER_ADMINISTATOR) | menu(Role.SYSTEM_ADMINISTATOR) | menu(Role.ADVISOR) or use 0, 1, 2 """
-    check_member = Action("check member", Role.ADVISOR, Crud.search_person, "member")
-    add_member = Action("add member", Role.ADVISOR, Crud.add_person, "member")
-    modify_member = Action("modify member", Role.ADVISOR,  Crud().modify_person, {'kind': "member"})
-    change_password = Action("change password", Role.ADVISOR, Crud.change_password, "advisor")
+    check_member = Action("check member", Role.ADVISOR, crud.search_person, {'kind': "member"})
+    add_member = Action("add member", Role.ADVISOR, crud.add_person, {'kind': "member"})
+    modify_member = Action("modify member", Role.ADVISOR,  crud.modify_person, {'kind': "member"})
+    change_password = Action("change password", Role.ADVISOR, crud.change_password, {'kind': "advisor"})
 
-    list_of_users = Action("list of users", Role.SYSTEM_ADMINISTATOR, Crud.check_users, None)
-    add_new_advisor = Action("add advisor", Role.SYSTEM_ADMINISTATOR, Crud.add_person, "advisor")
-    modify_advisor = Action("modify advisor", Role.SYSTEM_ADMINISTATOR, Crud().modify_person, {'kind': "advisor"})
-    delete_advisor = Action("delete advisor", Role.SYSTEM_ADMINISTATOR, Crud.delete_person, "advisor")
+    list_of_users = Action("list of users", Role.SYSTEM_ADMINISTATOR, crud.check_users, None)
+    add_new_advisor = Action("add advisor", Role.SYSTEM_ADMINISTATOR, crud.add_person, {'kind': "advisor"})
+    modify_advisor = Action("modify advisor", Role.SYSTEM_ADMINISTATOR, crud.modify_person, {'kind': "advisor"})
+    delete_advisor = Action("delete advisor", Role.SYSTEM_ADMINISTATOR, crud.delete_person, {'kind': "advisor"})
     # reset_advisor_password = Action("reset advisor password", Role.SYSTEM_ADMINISTATOR) ## not implemented
 
     # TODO: testen!!!
@@ -52,16 +53,16 @@ def menu(user_access_level: int):
     # restore_a_backup = Action("restore a backup", Role.SYSTEM_ADMINISTATOR, Helper.restore_backup)
     # see_logs = Action("see log(s)", Role.SYSTEM_ADMINISTATOR, Helper.see_logs)
 
-    delete_member = Action("delete member", Role.SYSTEM_ADMINISTATOR, Crud.delete_person, "member")
+    delete_member = Action("delete member", Role.SYSTEM_ADMINISTATOR, crud.delete_person, {'kind': "member"})
 
     # reset_system_admin_password = Action("reset system administator password", Role.SUPER_ADMINISTATOR) ## not
     # implemented
-    add_system_administrator = Action("add system administrator", Role.SUPER_ADMINISTATOR, Crud.add_person,
-                                      "systemadmin")
-    modify_system_administrator = Action("modify system administrator", Role.SUPER_ADMINISTATOR, Crud.modify_person,
-                                         "systemadmin")
-    delete_system_administrator = Action("delete system administrator", Role.SUPER_ADMINISTATOR, Crud.delete_person,
-                                         "systemadmin")
+    add_system_administrator = Action("add system administrator", Role.SUPER_ADMINISTATOR, crud.add_person,
+                                   {'kind': "systemadmin"})
+    modify_system_administrator = Action("modify system administrator", Role.SUPER_ADMINISTATOR, crud.modify_person,
+                                         {'kind': "systemadmin"})
+    delete_system_administrator = Action("delete system administrator", Role.SUPER_ADMINISTATOR, crud.delete_person,
+                                         {'kind': "systemadmin"})
 
     logout = Action("logout", Role.ADVISOR, UserInterface().main_screen, None)
 
